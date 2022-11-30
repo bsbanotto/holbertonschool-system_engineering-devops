51f0332609c20166b15094a5ee32d36996550755
root <ben.sbanotto@gmail.com>
2022-11-30 12:26:30 -0800

    First commit task 1

---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module to gather employee to do information from an API"""
+import csv
+import requests
+import sys
+
+
+def get_employee_todo():
+    """This method gathers employee to do information from an API
+
+    See README for display format
+    """
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+
+    employee_name = requests.get(url + 'users/{}'.format(employee_id))
+    employee_todo = requests.get(url + 'todos?userId={}'.format(employee_id))
+
+    employee_name = employee_name.json()
+    employee_todo = employee_todo.json()
+
+    with open('{}.csv'.format(employee_id), 'w', encoding='UTF8') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in employee_todo:
+            data = [employee_id, employee_name.get("username"),
+                    task.get("completed"), task.get("title")]
+            writer.writerow(data)
+
+if __name__ == "__main__":
+    get_employee_todo()
