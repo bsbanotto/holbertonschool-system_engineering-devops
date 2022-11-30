cd66181c2a09f90dd035641b54b247d16e2d0266
root <ben.sbanotto@gmail.com>
2022-11-30 15:49:11 -0800

    Passing variable name as string does not ever work

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -22,10 +22,11 @@ def everyone_todo_to_JSON():
         user_id = employee.get('id')
         return_dict[user_id] = []
         all_users_dict[user_id] = employee.get('username')
+    print(all_users_dict)
     for task in employees_todos:
         employee_tasks_dict = {}
         user_id = task.get('userId')
-        employee_tasks_dict['username'] = all_users_dict.get('userId')
+        employee_tasks_dict['username'] = all_users_dict.get(user_id)
         employee_tasks_dict['task'] = task.get('title')
         employee_tasks_dict['completed'] = task.get('completed')
         return_dict.get(user_id).append(employee_tasks_dict)
